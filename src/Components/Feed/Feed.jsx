61aba978eb93c734c111389a1e6f0f3ef50c8617
jAllanOli <j.a.o.huthan@gmail.com>
2025-01-05 11:29:43 -0300

    photo delete button added

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -9,7 +9,7 @@ export const Feed = () => {
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      <FeedPhotos setModalPhoto={setModalPhoto} />;
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 import { PhotoComments } from "./PhotoComments";
+import { PhotoDelete } from "./PhotoDelete";
 import styles from "./PhotoContent.module.css";
 
 export const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
-  console.log(comments);
+  const user = React.useContext(UserContext);
 
   return (
     <div className={styles.photo}>
@@ -14,7 +17,12 @@ export const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.vizualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
