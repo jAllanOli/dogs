4d9fe4e86de9b6fc9e95211beefc85bd6504e1ef
jAllanOli <j.a.o.huthan@gmail.com>
2025-01-07 20:02:51 -0300

    proptypes added to feed component

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FeedPhotos } from "./FeedPhotos";
 import { FeedModal } from "./FeedModal";
+import PropTypes, { string } from "prop-types";
 
 export const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState();
@@ -48,4 +49,12 @@ export const Feed = ({ user }) => {
       ))}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([string, number]),
 };
