451146b410bb675aa5487ba269129012e69d1339
jAllanOli <j.a.o.huthan@gmail.com>
2025-01-15 20:30:28 -0300

    footer styles added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,11 @@
+import Dogs from "../Assets/dogs-footer.svg?react";
 import styles from "./Footer.module.css";
 
 export const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
