22dfc294212e778c1719025b29b26f40ce116f62
jAllanOli <j.a.o.huthan@gmail.com>
2025-01-16 19:41:21 -0300

    graphs added

--- src/Pages/User/UserStats.jsx
+++ src/Pages/User/UserStats.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+import { Loading } from "../../utils/Loading";
+import { Error } from "../../utils/Error";
+import { Head } from "../../utils/Head";
+import { useFetch } from "../../Hooks/useFetch";
+import { STATS_GET } from "../../Api";
+
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
+
 export const UserStats = () => {
-  return <div>Estatísticas</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
